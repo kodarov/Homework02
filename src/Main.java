f1f29e9c9f516cff8c69e9dca97e8920ca848188
kodarov_salavat <kodarov@gmail.com>
2022-12-20 00:35:40 +0400

    ДЗ2\Задача 2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args)
     {
         task1();
+        System.out.println();
+        task2();
     }
     public static void task1 ()
     {
@@ -9,6 +11,19 @@ public class Main {
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
+        System.out.println("dog = " + dog);
+        System.out.println("cat = " + cat);
+        System.out.println("paper = " + paper);
+    }
+    public static void task2()
+    {
+        System.out.println("Задача 2");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
         System.out.println("dog = " + dog);
         System.out.println("cat = " + cat);
         System.out.println("paper = " + paper);
